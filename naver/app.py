a271e11a8915b0d4cefe20c56c66a101a5ceff6c
Kyung-Wan Woo <dnruddhks1223@gmail.com>
2024-01-16 08:28:13 -0800

    save to file

--- naver/app.py
+++ naver/app.py
@@ -1,22 +1,26 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
-import urllib.request
 
 def presentPrice(code):
     data = requests.get(f'https://finance.naver.com/item/sise.nhn?code={code}')
     soup = BeautifulSoup(data.content, 'html.parser')
-    return soup.find_all('strong', id="_nowVal")[0].text, soup.find_all('span', class_="tah")[5].text
+    print(soup.find_all('strong', id="_nowVal")[0].text)
+    print(soup.find_all('span', class_="tah")[5].text)
+    return soup.find_all('strong', id="_nowVal")[0].text
+    # prints out present price, trading volume of Compnay
 
-# prints out present price, trading volume of Samsung Electronics and LG Electronics
-Snow, Svol = presentPrice('005930')
-Lnow, Lvol = presentPrice('066575')
+companyCodes = ['005930', '066575', '005380', '035720', '034220', '003490']
+# Samsung, LG, Hyundai, Kakao, LG Display, Korean Air
 
 f = open('a.txt', 'w')
-f.write
+
+for i in range(6):
+    f.write( '\n' + presentPrice( companyCodes[i] ) )
 
 # css selector => class = .   id = #
 
-# # find em tag inside f_down class
+# # find esudo pip3 install requests
+#  tag inside f_down class
 # print( soup.select('.f_down em')[0].text )
 
 # # download image
