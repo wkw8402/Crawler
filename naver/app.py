8000215284b85c38288c4b09d9ae0c1240c1ebee
Kyung-Wan Woo <dnruddhks1223@gmail.com>
2024-01-15 23:58:56 -0800

    function

--- naver/app.py
+++ naver/app.py
@@ -2,21 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
-data = requests.get('https://finance.naver.com/item/sise.nhn?code=005930')
+def presentPrice(code):
+    data = requests.get(f'https://finance.naver.com/item/sise.nhn?code={code}')
+    soup = BeautifulSoup(data.content, 'html.parser')
+    return soup.find_all('strong', id="_nowVal")[0].text, soup.find_all('span', class_="tah")[5].text
 
-soup = BeautifulSoup(data.content, 'html.parser')
-print( soup.find_all('strong', id="_nowVal")[0].text )
-print( soup.find_all('span', class_="tah")[5].text )
+# prints out present price, trading volume of Samsung Electronics and LG Electronics
+Snow, Svol = presentPrice('005930')
+Lnow, Lvol = presentPrice('066575')
 
-# print out present price, trading volume of Samsung Electronics
+f = open('a.txt', 'w')
+f.write
 
 # css selector => class = .   id = #
 
-# find em tag inside f_down class
-print( soup.select('.f_down em')[0].text )
+# # find em tag inside f_down class
+# print( soup.select('.f_down em')[0].text )
 
-# download image
-image = soup.select('#img_chart_area')[0]
-print( image['src'] )
-urllib.request.urlretrieve(image['src'], 'image')
+# # download image
+# image = soup.select('#img_chart_area')[0]
+# print( image['src'] )
+# urllib.request.urlretrieve(image['src'], 'image')
 
